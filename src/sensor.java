ba855157cb0ea49fb1e1952fc92f1353a1000799
Adam Claassen <claassenadam@gmail.com>
2015-02-06 11:01:21 -0500

    wrote sensor class

--- src/sensor.java
+++ src/sensor.java
@@ -1,4 +1,18 @@
 
 public class sensor {
-
+	int pin;
+	int inputLow;
+	int inputHigh;
+	
+	public sensor(int pin, int inputLow, int inputHigh){
+		this.pin = pin;
+		this.inputLow = inputLow;
+		this.inputHigh = inputHigh;
+	}
+	public int read(){
+		return 0;
+	}
+	public int getPin(){
+		return this.pin;
+	}
 }
