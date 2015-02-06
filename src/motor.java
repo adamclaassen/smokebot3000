1fcad8c20e5cf9cccd00b8dc9af1b6bda4338a28
Adam Claassen <claassenadam@gmail.com>
2015-02-06 10:57:59 -0500

    wrote motor class

--- src/motor.java
+++ src/motor.java
@@ -1,6 +1,27 @@
 
 public class motor {
-	public motor(int pin){
-		
+	int pin;
+	int outputLow;
+	int outputHigh;
+	int speed;
+	
+	public motor(int pin, int outputLow, int outputHigh){
+		this.pin = pin;
+		this.outputLow = outputLow;
+		this.outputHigh = outputHigh;
 	}
+	
+	public void setSpeed(int speed){
+		this.speed = speed;
+		mapToOutput(speed);
+	}
+	public int getSpeed(){
+		return this.speed;
+	}
+	public int getPin(){
+		return this.pin;
+	}
+	private int mapToOutput(int x){
+		  return (x - 0) * (this.outputHigh - this.outputLow) / (100 - 0) + this.outputLow;
+		}
 }
