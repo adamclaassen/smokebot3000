c3157b1b206fd9bd5ce2d4099d913d7b4c366b9d
Adam Claassen <claassenadam@gmail.com>
2015-02-06 21:54:31 -0500

    wrote gpio class

--- src/robot/Gpio.java
+++ src/robot/Gpio.java
@@ -1,5 +1,49 @@
 package robot;
 
-public class Gpio {
+import java.io.IOException;
 
+public class Gpio {
+	private int pin;
+	private int value;
+	private String direction;
+	
+	public Gpio(int pin, String direction){
+		this.pin = pin;
+		this.direction = direction;
+		
+		try{
+			Process makePin = Runtime.getRuntime().exec(String.format("echo %d > /sys/class/gpio/export", this.pin));
+			makePin.waitFor();
+			Process setDir = Runtime.getRuntime().exec(String.format("echo %s > /sys/class/gpio/gpio%d/direction",this.direction));
+			setDir.waitFor();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	public Gpio(int pin, String direction, int value){
+		
+	}
+	
+	public void set(int value){
+		this.value = value;
+		try {
+			Process setValue = Runtime.getRuntime().exec(String.format("echo %d > /sys/class/gpio/value", this.value));
+			setValue.waitFor();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	public int get(){
+		return this.value;
+	}
+	public void destroy(){
+		Process destroy;
+		try {
+			destroy = Runtime.getRuntime().exec(String.format("echo %d > /sys/class/gpio/unexport", this.pin));
+			destroy.waitFor();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
