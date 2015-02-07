0772e8f48442312dbbfd103024a82c752b44e3d0
Adam Claassen <claassenadam@gmail.com>
2015-02-06 21:19:25 -0500

    changed sensor to interface, made class variables private

---
+++ src/analogSensor.java
@@ -0,0 +1,15 @@
+import robot.sensor;
+
+
+public class analogSensor implements sensor {
+	
+	private int pin;
+	
+	public analogSensor(int pin){
+		this.pin = pin;
+	}
+	public int read() {
+		return 0;
+	}
+
+}
