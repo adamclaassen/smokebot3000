bf173d61b9c95242b9960fe6a3e7e4b4192e98db
Adam Claassen <claassenadam@gmail.com>
2015-02-12 22:49:06 -0500

    tracked files mentioned in previous commit

---
+++ src/robot/ExampleRobot.java
@@ -0,0 +1,32 @@
+package robot;
+
+public class ExampleRobot extends Robot{
+	Motor left = new Motor(0, 0, 0);
+	Motor right = new Motor(0, 0, 0);
+	Sensor sensor = new ExampleSensor(1);
+
+	@Override
+	public void drive(int speed, int turn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dataCollect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void secondaryAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/robot/ExampleSensor.java
@@ -0,0 +1,14 @@
+package robot;
+
+public class ExampleSensor extends Sensor {
+	
+	public ExampleSensor(int pin){
+		super(0,0);
+	}
+	@Override
+	Object read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
