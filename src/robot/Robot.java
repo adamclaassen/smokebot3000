40d24f8bed182dfbe3e0aed6fd8c6954d83b3969
Adam Claassen <claassenadam@gmail.com>
2015-02-12 22:48:46 -0500

    added example sensor and robot extensions

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -10,7 +10,9 @@ public abstract class Robot {
 	
 	public abstract void drive(int speed, int turn);
 	
+	public abstract void dataCollect();
 	
+	public abstract void secondaryAction();
 	
-	
+	public abstract void test();
 }
