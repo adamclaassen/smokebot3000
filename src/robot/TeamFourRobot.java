05fe3e15c33e4c03416e072b2a8a7e3d44f30d4c
adamclaassen <claassenadam@gmail.com>
2015-03-13 22:02:05 -0400

    Moved team branch to team Robot impl.

---
+++ src/robot/TeamFourRobot.java
@@ -0,0 +1,35 @@
+package robot;
+
+public class TeamFourRobot extends Robot{
+
+	@Override
+	public void drive(int speed, int turn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dataCollect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void secondaryAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendSensorValue() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
