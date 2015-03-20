e8a7ee1de5f31cdf116c61d0fa5c88f294fee9a2
clevertoucan <diphthong@EROOS-C02NGFRYF8J2.local>
2015-03-20 14:34:05 -0400

    Added basic Applet implementation

---
+++ src/web/Applet.java
@@ -0,0 +1,14 @@
+package web;
+import java.applet.*;
+import java.awt.*;
+import javax.swing.*;
+import robot.Packet;
+
+public class Applet extends java.applet.Applet {
+
+	public Applet(){
+		
+	}
+	
+	
+}
