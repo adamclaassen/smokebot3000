9a64ecf40460b6bef3a999846487d8c8a58bc8eb
adamclaassen <claassenadam@gmail.com>
2015-03-24 00:37:09 -0400

    removed eclipse todo

--- src/robot/AnalogDigitalConverter.java
+++ src/robot/AnalogDigitalConverter.java
@@ -5,7 +5,6 @@ public class AnalogDigitalConverter extends Sensor{
 
 	public AnalogDigitalConverter(float inputHigh, float inputLow) {
 		super(inputHigh, inputLow);
-		// TODO Auto-generated constructor stub
 		
 		
 	}
