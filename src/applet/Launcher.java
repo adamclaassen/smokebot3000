29da1b68d3c82074eb56d6ca3e4bc1fa42830443
clevertoucan <clevertoucan@gmail.com>
2015-03-29 16:49:12 -0400

    added some stuff, but mostly am commiting so that I don't have to copy it over to windows manually because that's a pain.

---
+++ src/applet/Launcher.java
@@ -0,0 +1,11 @@
+package applet;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		OSVA applet = new OSVA();
+		applet.init();
+
+	}
+
+}
---
+++ src/applet/OSVA.java
@@ -0,0 +1,22 @@
+package applet;
+import robot.Packet;
+import javax.swing.*;
+import java.applet.*;
+import java.awt.*;
+
+
+public class OSVA extends java.applet.Applet{
+	int width;
+	int height;
+	
+	public void init(){
+		width  = this.getSize().width;
+		height = this.getSize().height;
+		this.setBackground(Color.black);
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+	
+}
