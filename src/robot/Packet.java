75bb6f4cd1eacf87985addff7b80a97173cef078
Adam Claassen <claassenadam@gmail.com>
2015-03-30 14:30:51 -0400

    rewrote Packet class

--- src/robot/Packet.java
+++ src/robot/Packet.java
@@ -1,64 +1,26 @@
 package robot;
 
-public class Packet {
-	static int nextPacketID = 0;
-	String rawPacket;
-	String messageType;
-	String data;
-	String osvPosition;
-	String packetID;
-	/*tentative packet format: <<packetID/%x%y%head/messageType/data>>
-	 * messageType rules:
-	 * POSITION: pass integer 0 for 'data', it is ignored
-	 * SENSOR_DATA: pass a Sensor object, it must have a properly implemented toString
-	 * BATT_STATUS: pass a battery voltage as a float (NOTE: may change in favor of passing batt sensor instance)
-	 * PATH: pass the Pathfinder object, this will generate a very large packet
-	 * CUSTOM_DATA_A: pass anything with a toString method, this is what the packet will contain
-	 * CUSTOM_DATA_B: see CUSTOM_DATA_A
-	 */
-	public Packet(String rawPacket){
-		
-	}
+public abstract class Packet{
+	
+	private String packet;
+	
 	public Packet(Position pos, MessageTypes mType, Object data){
-		
-		this.packetID = new String(Integer.toString(Packet.nextPacketID));
-		this.messageType = new String(mType.toString());
-		Packet.nextPacketID++;
-		this.osvPosition = new String(
-				"%"+
-				Integer.toString(pos.getX())+"%"+
-				Integer.toString(pos.getY())+"%"+
-				Float.toString(pos.getHead())
-				);
-		this.data = new String(data.toString());
-		/*switch(mType){
-		case POSITION:
-			this.data = new String("0");
-			break;
-		case SENSOR_DATA:
-			break;
-		case BATT_STATUS:
-			break;
-		case PATH:
-			break;
-		case CUSTOM_DATA_A:
-			this.data = new String(data.toString());
-			break;
-		case CUSTOM_DATA_B:
-			this.data = new String(data.toString());
-			break;
-		}
-		this.osvPosition.concat(">>");
-		*/
+		packet = new String("<<"+
+				pos.toString()+"/"+
+				mType.toString()+"/"+
+				data.toString()+"/"+
+				">>");
+	}
+	
+	public Packet(Position pos){
+		this(pos, MessageTypes.POSITION, 0);
+	}
+	
+	public Packet(Position pos, Exception e){
+		this(pos, MessageTypes.ERROR_MESSAGE, e.toString());
 	}
 	
 	public String toString(){
-		String output = new String(
-				"<<"+
-				Integer.toString(Packet.nextPacketID)+"/"+
-				this.osvPosition+"/"+
-				this.messageType+"/"+
-				this.data+">>");
-		return output;
+		return this.packet;
 	}
 }
