093979701108cb84e85df8e7170e325f38f9b43e
Adam Claassen <claassenadam@gmail.com>
2015-03-30 15:21:36 -0400

    added string decoder to packet class, needs finishingn

--- src/robot/Packet.java
+++ src/robot/Packet.java
@@ -4,6 +4,10 @@ public abstract class Packet{
 	
 	private String packet;
 	
+	Position pos;
+	MessageTypes mType;
+	Object data;
+	
 	public Packet(Position pos, MessageTypes mType, Object data){
 		packet = new String("<<"+
 				pos.toString()+"/"+
@@ -19,6 +23,46 @@ public abstract class Packet{
 	public Packet(Position pos, Exception e){
 		this(pos, MessageTypes.ERROR_MESSAGE, e.toString());
 	}
+	
+	public Packet(String incoming){
+		int posEnd;
+		int mTypeEnd;
+		int dataEnd;
+		
+		posEnd = incoming.indexOf("/");
+		mTypeEnd = incoming.indexOf("/", posEnd);
+		dataEnd = incoming.indexOf(">");
+		
+		String posString = incoming.substring(2, posEnd);
+		String mTypeString = incoming.substring(posEnd+1, mTypeEnd);
+		String dataString = incoming.substring(mTypeEnd+1, dataEnd);
+		
+		this.pos = new Position( //[143,445,449]
+				Integer.parseInt(posString.substring(1,2)),
+				Integer.parseInt(posString.substring(3,4)),
+				Integer.parseInt(posString.substring(5,6))
+				);
+		switch(mTypeString){
+		case "POSITION":
+			this.mType = MessageTypes.POSITION; break;
+		case "BATT_STATUS":
+			this.mType = MessageTypes.BATT_STATUS; break;
+		case "CUSTOM_DATA_A":
+			this.mType = MessageTypes.CUSTOM_DATA_A; break;
+		case "CUSTOM_DATA_B":
+			this.mType = MessageTypes.CUSTOM_DATA_B; break;
+		case "ERROR_MESSAGE":
+			this.mType = MessageTypes.ERROR_MESSAGE; break;
+		case "PATH":
+			this.mType = MessageTypes.PATH; break;
+		case "SENSOR_DATA":
+			this.mType = MessageTypes.SENSOR_DATA; break;
+		}
+		
+		data = this.decodeData(dataString);
+	}
+	
+	public abstract Object decodeData(String data);
 	
 	public String toString(){
 		return this.packet;
