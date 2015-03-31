b376a6ff3d170c7691f5f413d053c21d09776c4c
Adam Claassen <claassenadam@gmail.com>
2015-03-30 20:25:58 -0400

    changed packet(String) constructor to use a switch to select data decode method

--- src/robot/Packet.java
+++ src/robot/Packet.java
@@ -42,27 +42,22 @@ public abstract class Packet{
 				Integer.parseInt(posString.substring(3,4)),
 				Integer.parseInt(posString.substring(5,6))
 				);
-		switch(mTypeString){
-		case "POSITION":
-			this.mType = MessageTypes.POSITION; break;
-		case "BATT_STATUS":
-			this.mType = MessageTypes.BATT_STATUS; break;
-		case "CUSTOM_DATA_A":
-			this.mType = MessageTypes.CUSTOM_DATA_A; break;
-		case "CUSTOM_DATA_B":
-			this.mType = MessageTypes.CUSTOM_DATA_B; break;
-		case "ERROR_MESSAGE":
-			this.mType = MessageTypes.ERROR_MESSAGE; break;
-		case "PATH":
-			this.mType = MessageTypes.PATH; break;
-		case "SENSOR_DATA":
-			this.mType = MessageTypes.SENSOR_DATA; break;
+
+		this.mType = MessageTypes.valueOf(mTypeString);
+		
+		switch(this.mType){
+		case POSITION:      break;
+		case BATT_STATUS:   data = Integer.getInteger(dataString); break;
+		case SENSOR_DATA:   data = decodeSensorData(dataString); break;
+		case CUSTOM_DATA_A: data = decodeCustomA(dataString); break;
+		case CUSTOM_DATA_B: data = decodeCustomB(dataString); break;
 		}
 		
-		data = this.decodeData(dataString);
 	}
 	
-	public abstract Object decodeData(String data);
+	public abstract Object decodeSensorData(String data);
+	public abstract Object decodeCustomA(String data);
+	public abstract Object decodeCustomB(String data);
 	
 	public String toString(){
 		return this.packet;
