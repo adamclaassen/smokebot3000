4eae0b4a19aba67d18eaf7a2e3a90deb015e70f9
clevertoucan <clevertoucan@gmail.com>
2015-04-03 10:59:39 -0700

    Finished a WORKING OSV Applet, ready for packet integration

--- src/applet/OSVA.java
+++ src/applet/OSVA.java
@@ -1,35 +1,37 @@
 package applet;
 import robot.*;
+
 import javax.swing.*;
-import java.applet.*;
+
 import java.awt.*;
 
-
-public class OSVA extends java.applet.Applet{
-	int width;
-	int height;
+public class OSVA extends JApplet{
+	static int width;
+	static int height;
 	
 	public void init(){
-		//connect to server somehow, get initial packet 
-		width  = this.getSize().width;
-		height = this.getSize().height;
-		this.setBackground(Color.black);
+		width = this.getWidth();
+		height = this.getHeight();
 		
+		AppletField field = new AppletField("Hello World");	//Creates a new AppletField, field. AppletField is a default content pane for displaying information on the applet
+		field.setBackground(new Color(0,0,0));
+		field.setForeground(new Color(255,255,255));
+		this.setContentPane(field);						//Sets the Applet's content pane to be the default AppletField. 
 		
 	}
 	
-	public void paint(Graphics g){
-		g.setFont(Font.SANS_SERIF.);
-		//retrieve packet (getPacket) and draw strings, generate map overlay once path is determined
-		Packet data = getPacket("192.168.0.11");
-		g.drawString(data.toString(), 0, 0);
+	public void stop(){
+		
+	}
+	
+	public void destroy(){
+		
 	}
 	
 	public Packet getPacket(String ip){
 		//Do internety things
-		Packet defaultPacket = new Packet("<<[1,2,2]>>");
+		Packet defaultPacket = null;
 		return defaultPacket;
-		
 	}
 	
 }
