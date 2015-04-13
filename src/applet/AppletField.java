637a5321c9cfd8d2fcffad9246551c79c9ec5b34
clevertoucan <clevertoucan@gmail.com>
2015-04-13 16:54:29 -0400

    Fixed issues with applet, needs self-signed stuff

--- src/applet/AppletField.java
+++ src/applet/AppletField.java
@@ -3,8 +3,12 @@ import javax.swing.*;
 
 import java.awt.*;
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.URL;
+
+import org.apache.commons.io.*;
 //This is a default OSVA Component used for displaying
 //information on the applet. Individual teams will have
 //to implement the Field themselves, so that they can 
@@ -14,10 +18,25 @@ import java.net.URL;
 
 
 public class AppletField extends JPanel{
+
+	private static final long serialVersionUID = -6824103810267271345L;
 	String info;
 	
 	public AppletField(URL xml){
-		InputStream in = new BufferedInputStream(xml.openStream());
+		try {
+			InputStream in = new BufferedInputStream(xml.openStream());
+			StringWriter writer = new StringWriter();
+			IOUtils.copy(in,writer,"UTF-8");
+			info = writer.toString();
+		} 
+		catch (IOException e) {
+			info = "APPLETFIELD ERROR: " + e.getMessage();
+		}
+		
+	}
+	
+	public AppletField(String data){
+		info = data;
 	}
 	
 	public void paintComponent(Graphics g){
--- src/applet/OSVA.java
+++ src/applet/OSVA.java
@@ -1,21 +1,34 @@
 package applet;
-import robot.*;
+
 import javax.swing.*;
-import comm.Packet;
-import java.applet.*;
-import java.awt.*;
 
 import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class OSVA extends JApplet{
+
+	private static final long serialVersionUID = 2226215276161285417L;
 	static int width;
 	static int height;
 	
 	public void init(){
 		width = this.getWidth();
 		height = this.getHeight();
+		URL source = null;
+		try {
+			source = new URL("http://192.168.1.161");
+		} 
+		catch (MalformedURLException e) {
+			
+			String info = "APPLET INITIALIZATION ERROR: " + e.getMessage();
+			AppletField field = new AppletField(info);
+			field.setBackground(new Color(0,0,0));
+			field.setForeground(new Color(255,255,255));
+			this.setContentPane(field);
+		}
 		
-		AppletField field = new AppletField("Hello World");	//Creates a new AppletField, field. AppletField is a default content pane for displaying information on the applet
+		AppletField field = new AppletField(source);	//Creates a new AppletField, field. AppletField is a default content pane for displaying information on the applet
 		field.setBackground(new Color(0,0,0));
 		field.setForeground(new Color(255,255,255));
 		this.setContentPane(field);						//Sets the Applet's content pane to be the default AppletField. 
