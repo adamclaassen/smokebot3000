159f158fc0eb220e87b0a084fa3a63d35506b89c
Vinai Rachakonda <vinaichand@yahoo.com>
2015-04-16 15:09:41 -0400

    Changed Serial Wrapper

--- src/comm/SerialWrapper.java
+++ src/comm/SerialWrapper.java
@@ -9,55 +9,68 @@ import com.pi4j.io.serial.*;
 public class SerialWrapper{
 	
 	final Serial serial;
-	
-	private ArrayList<String> messages;
-	
-	public SerialWrapper(){
-		this.serial = SerialFactory.createInstance();
-		serial.addListener( new SerialDataEventListener() {
+	//private ArrayList<String> messages;
+
+	public  SerialWrapper() {
+		serial = SerialFactory.createInstance();
+		serial.addListener(new SerialDataEventListener() {
 			@Override
 			public void dataReceived(SerialDataEvent event) {
 				try {
-					messages.add(event.getString(Charset.defaultCharset()));
+					System.out.println("We got some data");
+					System.out.print("[HEX DATA]   " + event.getHexByteString());
+					System.out.print("[ASCII DATA] " + event.getAsciiString());
 				} catch (IOException e) {
-					robot.SimpleRobot.eHandler.addError(e);
+					e.printStackTrace();
 				}
 			}
+		});
+	}
+
+	public void read() {
+		try {
+			System.out.println("Message: " + serial.read());
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		);
+		
+
 	}
+
+	public void write(String message) {
+		 try {
+	            // open the default serial port provided on the GPIO heade
+	            serial.open(Serial.DEFAULT_COM_PORT, Baud._38400, DataBits._8, Parity.NONE, StopBits._1, FlowControl.NONE);
+	           if (serial.isOpen())
+	        	   System.out.println("Serial is open");
+	           else{
+	        	   System.out.println("Not open");
+	           }
+	                       
+	                try {
+	                    // write a formatted string to the serial transmit buffer
+	                    serial.write(message);
+	    
+	                               }
+	                catch(IllegalStateException ex){
+	                    ex.printStackTrace();                    
+	                }
+	                
+	                // wait 1 second before continuing
+	                Thread.sleep(1000);
+	            
+	            
+	        }
+	        catch(IOException | InterruptedException ex) {
+	            System.out.println(" ==>> SERIAL SETUP FAILED : " + ex.getMessage());
+	            return;
+	        }
 	
-	/**
-	 * Returns the last message received, then clears it
-	 * @return String
-	 */
-	public String getNewestMessage(){
-		String temp = this.messages.get(this.messages.size()-1);
-		this.messages.remove(this.messages.size()-1);
-		return temp;
-	}
-	
-	/**
-	 * Returns the oldest message, then clears it
-	 * @return String
-	 */
-	public String getOldestMessage(){
-		String temp = this.messages.get(0);
-		this.messages.remove(0);
-		return temp;
-	}
-	
-	/**
-	 * Returns ArrayList of all messages, then clears them.
-	 * @return ArrayList of Strings
-	 */
-	public ArrayList<String> getAllMessages(){
-		ArrayList<String> temp = this.messages;
-		this.messages.clear();
-		return temp;
-	}
-	
-	public void clearMessages(){
-		this.messages.clear();
+		 
+
 	}
 }
