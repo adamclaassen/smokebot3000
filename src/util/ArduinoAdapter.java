7b96521e9167dea12032b69dc94f3923dfec051f
Adam Claassen <claassenadam@gmail.com>
2015-05-01 02:06:08 -0400

    ffs

--- src/util/ArduinoAdapter.java
+++ src/util/ArduinoAdapter.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class ArduinoAdapter {
@@ -8,31 +9,22 @@ public class ArduinoAdapter {
 	int fd = -1;
 	
 	public ArduinoAdapter(){
-		File dev = new File("/dev/");
-		for(String device:dev.list()){
-			if(device.contains("ttyACM")){
-				fd = robot.SimpleRobot.serial.serialOpen("/dev/" + device, 115200);
-			}
-		}
-		if(fd == -1){
-			fd = robot.SimpleRobot.serial.serialOpen("/dev/ttyACM0", 115200);
-		}
 	}
 	
 	public double readData(){
-		robot.SimpleRobot.serial.serialFlush(fd);
-		robot.SimpleRobot.serial.serialPuts(fd, "<r//>");
-		
-		int readCount = 0;
-		while(robot.SimpleRobot.serial.serialDataAvail(fd)<=5){
-			if(readCount>100){
-				robot.SimpleRobot.serial.serialFlush(fd);
-				robot.SimpleRobot.serial.serialPuts(fd,  "<r//>");
-				readCount = 0;
-			}
-			readCount++;
+		try {
+			robot.SimpleRobot.serial.flush();
+			
+		} catch (IllegalStateException | IOException e1) {
+			e1.printStackTrace();
 		}
-		byte[] serialBytes = robot.SimpleRobot.serial.serialGetAvailableBytes(fd);
+		
+		byte[] serialBytes = null;
+		try {
+			serialBytes = robot.SimpleRobot.serial.read();
+		} catch (IllegalStateException | IOException e) {
+			robot.SimpleRobot.eHandler.addError(e);
+		}
 		char[] serialChars = new char[serialBytes.length];
 		for(int i = 0; i<serialBytes.length; i++){
 			serialChars[i] = (char) serialBytes[i];
@@ -41,29 +33,30 @@ public class ArduinoAdapter {
 		return Double.parseDouble(new String(serialChars).split("<|>")[1].split("/")[1]);
 	}
 	
-	@SuppressWarnings("deprecation")
 	public boolean setMotorSpeed(int pin, double speed){
-		robot.SimpleRobot.serial.serialFlush(fd);
-		robot.SimpleRobot.serial.serialPuts(fd, String.format("<m/{0}/{1}>", String.format("%05d",pin), String.format("%05d",speed)));
-		
-		int readCount = 0;
-		while(robot.SimpleRobot.serial.serialDataAvail(fd)<=5){
-			if(readCount>100){
-				robot.SimpleRobot.serial.serialFlush(fd);
-				robot.SimpleRobot.serial.serialPuts(fd, String.format("<m/{0}/{1}>", String.format("%05d",pin), String.format("%05d",speed)));
-				readCount = 0;
+		byte[] serialChars;
+		try {
+			robot.SimpleRobot.serial.flush();;
+			robot.SimpleRobot.serial.write(String.format("<m/{0}/{1}>", String.format("%05d",pin), String.format("%05d",speed)).getBytes());
+			
+			int readCount = 0;
+			while(robot.SimpleRobot.serial.available()<=5){
+				if(readCount>100){
+					robot.SimpleRobot.serial.flush();;
+					robot.SimpleRobot.serial.write(String.format("<m/{0}/{1}>", String.format("%05d",pin), String.format("%05d",speed)).getBytes());
+					readCount = 0;
+				}
+				readCount++;
 			}
-			readCount++;
+			
+			serialChars = robot.SimpleRobot.serial.read();
+			if(serialChars[1] == 'a'){
+				return true;
+			}
+			return setMotorSpeed(pin, speed);
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
 		}
-		char[] serialChars = new char[robot.SimpleRobot.serial.serialDataAvail(fd)];
-		int i = 0;
-		while(robot.SimpleRobot.serial.serialDataAvail(fd)>0){
-			serialChars[i] = (char) robot.SimpleRobot.serial.serialGetchar(fd);
-		}
-		if(serialChars[1] == 'a'){
-			return true;
-		}
-		return setMotorSpeed(pin, speed);
-		
+		return false;
 	}
 }
