33daa90b8bdfc7532ae95c07a3cdcf5c34545ad3
clevertoucan <clevertoucan@gmail.com>
2015-05-07 01:40:28 -0700

    sadfasdfsdfasdfasdF

--- src/comm/ArduinoAdapter.java
+++ src/comm/ArduinoAdapter.java
@@ -19,10 +19,10 @@ public class ArduinoAdapter {
 		boolean ack = false;
 		int readCount = 0;
 		try {
-			//robot.SimpleRobot.arduinoSerial.write(msg);
+			robot.SimpleRobot.arduinoSerial.write(msg);
 			System.out.println("The serial write happened");
 			while(robot.SimpleRobot.arduinoSerial.available()<5){
-				System.out.println("nada");
+				System.out.println("Still waiting for serial data return");
 			}
 			if(robot.SimpleRobot.arduinoSerial.read().equals("<a//>")){
 				return true;
